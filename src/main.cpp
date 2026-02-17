023d74e7b01bfbc35f5ccaaac6d593b17d8ed994
x8laye4r <disposal@x8laye4r.dev>
2026-02-17 22:31:41 +0100

    Implementation of GraphQL-Queries - Implemented FuzzyDate fragment - Implemented Staff Queries - Implemented User Favorite Queries - Implemented Studio Queries - Implemented Toggle Favorite

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
+#include <iostream>
 #include <QApplication>
 #include <anilex/ui/MainWindow.h>
 #include <version.h>
+#include <QtGlobal>
+#include <QDebug>
+#include <QDir>
+#include <qdiriterator.h>
+#include <QFile>
+#include <QString>
 
 int main(int argc, char *argv[]) {
     QApplication app_anilex(argc, argv);
@@ -12,6 +19,17 @@ int main(int argc, char *argv[]) {
 
     // qInfo() << app_anilex.applicationVersion();
     // qInfo() << QStandardPaths::writableLocation(QStandardPaths::CacheLocation);
+    qInfo() << QT_VERSION_STR;
+
+    QFile file(":/graphql/activity/ToggleActivitySubcriptionMutation.graphql");
+
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        qWarning() << "Failed to open file:" << file.errorString();
+        return 0;
+    }
+    QString input = file.readAll();
+    qInfo().noquote() << input;
+
 
     MainWindow widget;
     widget.show();
