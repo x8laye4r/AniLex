d7dc7f308773e81c44e747dee1ad8e089e30be94
x8laye4r <contact@mail.x8laye4r.net>
2026-06-06 19:02:21 +0200

    feat(CMake) Add translation files for English and German languages

---
+++ assets/translations/anilex_de.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>AuthErrors</name>
+    <message>
+        <location filename="../../include/anilex/core/AuthErrors.h" line="19"/>
+        <source>No error accoured</source>
+        <translation>Kein Fehler ist aufgetreten</translation>
+    </message>
+    <message>
+        <location filename="../../include/anilex/core/AuthErrors.h" line="21"/>
+        <source>Failed to start the server</source>
+        <translation>Server starten fehlgeschlagen</translation>
+    </message>
+    <message>
+        <location filename="../../include/anilex/core/AuthErrors.h" line="23"/>
+        <source>Failed to save token to the keyring</source>
+        <translation>Verifizierungscode speichern fehlgeschlagen</translation>
+    </message>
+    <message>
+        <location filename="../../include/anilex/core/AuthErrors.h" line="25"/>
+        <source>Failed to fetch user</source>
+        <translation>Benutzer abfragen fehlgeschlagen</translation>
+    </message>
+    <message>
+        <location filename="../../include/anilex/core/AuthErrors.h" line="27"/>
+        <source>Unknown error</source>
+        <translation>Unbekannter Fehler</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../src/main.cpp" line="82"/>
+        <source>Success</source>
+        <translation>Erfolg</translation>
+    </message>
+    <message>
+        <location filename="../../src/main.cpp" line="82"/>
+        <source>Token saved! You can now browse your list!</source>
+        <translation>Verifizierungscode gespeichert! Du kannst nun dein Liste durchsuchen!</translation>
+    </message>
+    <message>
+        <location filename="../../src/main.cpp" line="84"/>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+</context>
+</TS>
---
+++ assets/translations/anilex_en.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+<context>
+    <name>AuthErrors</name>
+    <message>
+        <location filename="../../include/anilex/core/AuthErrors.h" line="19"/>
+        <source>No error accoured</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../include/anilex/core/AuthErrors.h" line="21"/>
+        <source>Failed to start the server</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../include/anilex/core/AuthErrors.h" line="23"/>
+        <source>Failed to save token to the keyring</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../include/anilex/core/AuthErrors.h" line="25"/>
+        <source>Failed to fetch user</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../include/anilex/core/AuthErrors.h" line="27"/>
+        <source>Unknown error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../src/main.cpp" line="82"/>
+        <source>Success</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/main.cpp" line="82"/>
+        <source>Token saved! You can now browse your list!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/main.cpp" line="84"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
